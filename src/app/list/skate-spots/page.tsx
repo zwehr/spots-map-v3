b5b9eca58255b0c778bdba5716519f99f9e37da3
zwehr <zwehrmeister@gmail.com>
2023-06-29 23:44:27 -0400

    add spot data to table

--- src/app/list/skate-spots/page.tsx
+++ src/app/list/skate-spots/page.tsx
@@ -7,9 +7,48 @@ export default async function SpotsList() {
   return (
     <>
       <h1>Spots List</h1>
-      {spots.map((spot) => (
-        <p key={spot._id}>{spot.name}</p>
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+            <th>Type</th>
+            <th>Status</th>
+            <th>Tags</th>
+            <th>YouTube Links</th>
+            <th>Date Added</th>
+          </tr>
+        </thead>
+        <tbody>
+          {spots.map((spot) => (
+            <tr key={spot._id}>
+              <td>{spot.name}</td>
+              <td>{spot.description}</td>
+              <td>{spot.lat.toFixed(3)}</td>
+              <td>{spot.lng.toFixed(3)}</td>
+              <td>{spot.type}</td>
+              <td>{spot.status}</td>
+              <td>{spot.tags}</td>
+              <td>
+                {spot.youtubeLinks.map((link, index) => (
+                  <>
+                    <a
+                      href={link}
+                      target='_blank'
+                      className='underline text-sky-600 hover:text-sky-800'
+                    >
+                      {index + 1}
+                    </a>{' '}
+                  </>
+                ))}
+              </td>
+              <td>{new Date(spot.createdAt).toLocaleDateString('en-US')}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
