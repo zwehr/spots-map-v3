8f854119833b72f3f5691d1efa391a0703afbee9
zwehr <zwehrmeister@gmail.com>
2023-06-30 21:54:18 -0400

    show separate "no spots found matching..." message on list/[tag] page when no spots are found

--- src/app/list/skate-spots/tag/[tag]/page.tsx
+++ src/app/list/skate-spots/tag/[tag]/page.tsx
@@ -15,43 +15,51 @@ export default async function SpotsListByTag({
   return (
     <>
       <h1>Spots List</h1>
-      <h2>
-        {spots?.length} spots found matching tag: {decodeURI(params.tag)}
-      </h2>
-      <table className='max-w-6xl mx-auto mt-6'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Type</th>
-            <th>Tags</th>
-            <th>Added</th>
-            <th>More</th>
-          </tr>
-        </thead>
-        <tbody>
-          {spots &&
-            spots.map((spot) => (
-              <tr key={spot._id}>
-                <td className='font-semibold'>{spot.name}</td>
-                <td>{spot.description}</td>
-                <td>{spot.type[0].toUpperCase() + spot.type.slice(1)}</td>
-                <td>
-                  <LinkTags tags={spot.tags} />
-                </td>
-                <td>{new Date(spot.createdAt).toLocaleDateString('en-US')}</td>
-                <td>
-                  <Link
-                    href={`../spot/${spot._id}`}
-                    className='link font-semibold'
-                  >
-                    More Info
-                  </Link>
-                </td>
+      {spots && spots.length > 0 ? (
+        <>
+          <h2>
+            {spots?.length} spots found matching tag: {decodeURI(params.tag)}
+          </h2>
+          <table className='max-w-6xl mx-auto mt-6'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Type</th>
+                <th>Tags</th>
+                <th>Added</th>
+                <th>More</th>
               </tr>
-            ))}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {spots &&
+                spots.map((spot) => (
+                  <tr key={spot._id}>
+                    <td className='font-semibold'>{spot.name}</td>
+                    <td>{spot.description}</td>
+                    <td>{spot.type[0].toUpperCase() + spot.type.slice(1)}</td>
+                    <td>
+                      <LinkTags tags={spot.tags} />
+                    </td>
+                    <td>
+                      {new Date(spot.createdAt).toLocaleDateString('en-US')}
+                    </td>
+                    <td>
+                      <Link
+                        href={`../spot/${spot._id}`}
+                        className='link font-semibold'
+                      >
+                        More Info
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <h2>No spots matching tag: {params.tag}</h2>
+      )}
     </>
   );
 }
