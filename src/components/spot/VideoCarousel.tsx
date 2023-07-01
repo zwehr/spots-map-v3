482bda196eaa8226d73f1121d31b263c920bdfb6
zwehr <zwehrmeister@gmail.com>
2023-06-30 21:16:13 -0400

    add timestamp to VideoCarousel

--- src/components/spot/VideoCarousel.tsx
+++ src/components/spot/VideoCarousel.tsx
@@ -10,6 +10,15 @@ type VideoProps = {
 export default function VideoCarousel({ youtubeLinks }: VideoProps) {
   const [youtubeLinkIndex, setYoutubeLinkIndex] = useState(0);
 
+  // Takes total seconds from end of YouTube link (after '='), converts to number, and takes floor of division by 60 (seconds)
+  let currTimeStampMinute = Math.floor(
+    parseInt(youtubeLinks[youtubeLinkIndex].split('=')[1]) / 60
+  );
+
+  // Similar to above, but % operator gives seconds
+  let currTimeStampSeconds =
+    parseInt(youtubeLinks[youtubeLinkIndex].split('=')[1]) % 60;
+
   const handlePreviousVideoClick = () => {
     if (youtubeLinkIndex === 0) {
       setYoutubeLinkIndex(youtubeLinks.length - 1);
@@ -47,6 +56,12 @@ export default function VideoCarousel({ youtubeLinks }: VideoProps) {
           allowFullScreen
         ></iframe>
         <p>{`Video ${youtubeLinkIndex + 1}/${youtubeLinks.length}`}</p>
+        <p>
+          Spot Timestamp: {currTimeStampMinute}:
+          {/* If timestamp seconds is less than ten, we need to manually add a 0 (e.g. 5:02) */}
+          {currTimeStampSeconds < 10 && '0'}
+          {currTimeStampSeconds}
+        </p>
         <p></p>
       </div>
       <div>
