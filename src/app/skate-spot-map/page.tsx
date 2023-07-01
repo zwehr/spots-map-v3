dbf4a84ed4dc90002af6b742afb1b8a61432ef65
zwehr <zwehrmeister@gmail.com>
2023-07-01 03:10:18 -0400

    load simple Google Map (no data)

--- src/app/skate-spot-map/page.tsx
+++ src/app/skate-spot-map/page.tsx
@@ -1,14 +1,15 @@
 import MapList from '@/components/map/MapList';
 import GoogleMapFull from '@/components/map/GoogleMapFull';
+import getAllSpots from '@/lib/fetch/getAllSpots';
 
-export default function Map() {
+export default async function Map() {
   return (
     <>
-      <div className='flex h-screen w-screen flex-col lg:flex-row bg-rose-100'>
-        <div className='lg:w-1/3 h-full bg-emerald-100'>
+      <div className='flex h-screen w-screen flex-col-reverse lg:flex-row'>
+        <div className='lg:w-1/3 h-full'>
           <MapList />
         </div>
-        <div className='lg:w-2/3 h-full bg-violet-100'>
+        <div className='lg:w-2/3 h-full'>
           <GoogleMapFull />
         </div>
       </div>
--- src/components/map/GoogleMapFull.jsx
+++ src/components/map/GoogleMapFull.jsx
@@ -1,3 +1,24 @@
+'use client';
+
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
 export default function GoogleMapFull() {
-  return <>GoogleMapFull</>;
+  return (
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}>
+      <GoogleMap
+        //mapContainerStyle={}
+        center={center}
+        zoom={10}
+        mapContainerClassName='w-full h-full'
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  );
 }
