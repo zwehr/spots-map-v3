c9b5eb4646826557f2b59572b35292c0bc0fee5a
zwehr <zwehrmeister@gmail.com>
2023-07-02 22:14:45 -0400

    show number of spots on map list

--- src/components/map/MapList.tsx
+++ src/components/map/MapList.tsx
@@ -8,7 +8,11 @@ type MapListProps = {
 export default function MapList({ spots, selectedSpot }: MapListProps) {
   return (
     <div className='h-full overflow-y-scroll'>
-      <h2>hi</h2>
+      <p className='ml-4 mt-2 text-xl'>
+        {spots && spots.length > 0
+          ? `${spots.length} spots found.`
+          : 'No spots found. Try searching another area'}
+      </p>
       {spots &&
         spots.map((spot) => (
           <MapListItem spot={spot} selectedSpot={selectedSpot} key={spot._id} />
