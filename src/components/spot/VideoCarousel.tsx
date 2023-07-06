28c1ab66c9b410902d28b3e01340ace42d0b7a99
zwehr <zwehrmeister@gmail.com>
2023-07-06 04:12:40 -0400

    show appropriate messages when there are bad or no YouTube links

--- src/components/spot/VideoCarousel.tsx
+++ src/components/spot/VideoCarousel.tsx
@@ -27,44 +27,53 @@ export default function VideoCarousel({ youtubeLinks }: VideoProps) {
     }
   };
 
-  return (
-    <div className='flex my-5 mx-auto'>
-      {youtubeLinks.length > 1 && (
-        <div>
-          <button
-            className='mt-36 p-1 text-4xl rounded-full shadow-lg bg-slate-100 hover:bg-slate-200'
-            onClick={handlePreviousVideoClick}
-          >
-            <GrPrevious />
-          </button>
+  if (youtubeLinks.length === 0) {
+    return (
+      <p className='text-center font-semibold'>[No youtube links found.]</p>
+    );
+  } else if (!youtubeLinks[youtubeLinkIndex].includes('start=')) {
+    return (
+      <p className='text-center font-semibold'>[YouTube link(s) invalid.]</p>
+    );
+  } else
+    return (
+      <div className='flex my-5 mx-auto'>
+        {youtubeLinks.length > 1 && (
+          <div>
+            <button
+              className='mt-36 p-1 text-4xl rounded-full shadow-lg bg-slate-100 hover:bg-slate-200'
+              onClick={handlePreviousVideoClick}
+            >
+              <GrPrevious />
+            </button>
+          </div>
+        )}
+        <div className='w-[550px] mx-auto text-center'>
+          <iframe
+            /* Default width and height are 560 and 315 respectively. */
+            width='550'
+            height='315'
+            src={youtubeLinks[youtubeLinkIndex]}
+            title='YouTube video player'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            allowFullScreen
+          ></iframe>
+          <p>{`Video ${youtubeLinkIndex + 1}/${youtubeLinks.length}`}</p>
+          <p>
+            Spot Timestamp: {youtubeToTimestamp(youtubeLinks[youtubeLinkIndex])}
+          </p>
+          <p></p>
         </div>
-      )}
-      <div className='w-[550px] mx-auto text-center'>
-        <iframe
-          /* Default width and height are 560 and 315 respectively. */
-          width='550'
-          height='315'
-          src={youtubeLinks[youtubeLinkIndex]}
-          title='YouTube video player'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          allowFullScreen
-        ></iframe>
-        <p>{`Video ${youtubeLinkIndex + 1}/${youtubeLinks.length}`}</p>
-        <p>
-          Spot Timestamp: {youtubeToTimestamp(youtubeLinks[youtubeLinkIndex])}
-        </p>
-        <p></p>
+        {youtubeLinks.length > 1 && (
+          <div>
+            <button
+              className='mt-36 p-1 text-4xl rounded-full shadow-lg bg-slate-100 hover:bg-slate-200'
+              onClick={handleNextVideoClick}
+            >
+              <GrNext />
+            </button>
+          </div>
+        )}
       </div>
-      {youtubeLinks.length > 1 && (
-        <div>
-          <button
-            className='mt-36 p-1 text-4xl rounded-full shadow-lg bg-slate-100 hover:bg-slate-200'
-            onClick={handleNextVideoClick}
-          >
-            <GrNext />
-          </button>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
