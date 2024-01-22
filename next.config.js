0c374cf116b8bf768debeddadb78d4f1620cc30d
zwehr <zwehrmeister@gmail.com>
2024-01-21 21:25:21 -0500

    update to Next 14, remove experimental server actions from next.config now that they are supported

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     domains: ['skate-tourism-spot-images.s3.us-east-2.amazonaws.com'],
   },
