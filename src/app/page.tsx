7679a902436ea32af6b8b43dc33a130c8e899b1a
zwehr <zwehrmeister@gmail.com>
2024-01-28 21:45:03 -0500

    generate types (automatically from Supabase)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,88 +10,154 @@ export default function Home() {
     <div className='max-w-screen-xl mx-auto my-4 shadow-lg'>
       <h1 className='hidden'>Home</h1>
       <ImageGrid></ImageGrid>
-      <div className='recent-and-description flex h-96 bg-gradient-to-r from-blue-100 from-20% via-neutral-200 via-40% to-emerald-200 to-90%'>
-        <div className='recent-spots w-1/3 ml-4 mr-2 my-4 rounded-lg overflow-scroll bg-slate-50 shadow-md'>
+      <div className='recent-and-descriptions mt-4 flex'>
+        <div className='recent-spots-container w-1/3'>
           <h2 className='text-left'>Recent Spots</h2>
-          <div>
-            <div className='m-2'>
-              <h3>Spot Name in City Name</h3>
-              <Image
-                className='rounded-md'
-                src='/handrail.jpeg'
-                width={500}
-                height={500}
-                alt='Skatespot photo'
-              />
+          <div className='recent-spots ml-4 mr-2 my-4 rounded-lg overflow-scroll bg-slate-50 shadow-md'>
+            <div>
+              <div className='m-2'>
+                <h3>Spot Name in City Name</h3>
+                <Image
+                  className='rounded-md'
+                  src='/handrail.jpeg'
+                  width={500}
+                  height={500}
+                  alt='Skatespot photo'
+                />
+              </div>
             </div>
-          </div>
-          <div>
-            <div className='m-2'>
-              <h3>Spot Name in City Name</h3>
-              <Image
-                className='rounded-md'
-                src='/handrail.jpeg'
-                width={500}
-                height={500}
-                alt='Skatespot photo'
-              />
+            <div>
+              <div className='m-2'>
+                <h3>Spot Name in City Name</h3>
+                <Image
+                  className='rounded-md'
+                  src='/handrail.jpeg'
+                  width={500}
+                  height={500}
+                  alt='Skatespot photo'
+                />
+              </div>
             </div>
-          </div>
-          <div>
-            <div className='m-2'>
-              <h3>Spot Name in City Name</h3>
-              <Image
-                className='rounded-md'
-                src='/handrail.jpeg'
-                width={500}
-                height={500}
-                alt='Skatespot photo'
-              />
+            <div>
+              <div className='m-2'>
+                <h3>Spot Name in City Name</h3>
+                <Image
+                  className='rounded-md'
+                  src='/handrail.jpeg'
+                  width={500}
+                  height={500}
+                  alt='Skatespot photo'
+                />
+              </div>
             </div>
-          </div>
-          <div>
-            <div className='m-2'>
-              <h3>Spot Name in City Name</h3>
-              <Image
-                className='rounded-md'
-                src='/handrail.jpeg'
-                width={500}
-                height={500}
-                alt='Skatespot photo'
-              />
+            <div>
+              <div className='m-2'>
+                <h3>Spot Name in City Name</h3>
+                <Image
+                  className='rounded-md'
+                  src='/handrail.jpeg'
+                  width={500}
+                  height={500}
+                  alt='Skatespot photo'
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className='what-we-offer w-2/3 ml-2 mr-4 my-4 rounded-lg bg-slate-50 shadow-md'>
-          <h2 className='text-left'>
-            A collection of <NumberCountUp count={123} /> spots, and more...
-          </h2>
-          <ul className='ml-4'>
-            <li className='flex m-2'>
-              <FaSearch />
-              <p className={`pl-1 ${merriweather.className}`}>
-                Robust tagging and search system.
-              </p>
-            </li>
-            <li className='flex m-2'>
-              <FaYoutube />
-              <p className={`pl-1 ${merriweather.className}`}>
-                Weekly additions from the newest video releases.
-              </p>
-            </li>
-            <li className='flex m-2'>
-              <GiSkateboard />
-              <p className={`pl-1 ${merriweather.className}`}>
-                Created and curated by skaters.
-              </p>
-            </li>
-            <li className='flex m-2'>
-              <FaHeart />
-              <p className={`pl-1 ${merriweather.className}`}>
-                Save your favorite spots.
-              </p>
-            </li>
-          </ul>
+        <div className='descriptions-container w-2/3'>
+          <div className='what-we-offer ml-2 mr-4 my-4 rounded-lg bg-slate-50 shadow-md'>
+            <h2 className='text-left'>
+              A collection of <NumberCountUp count={123} /> spots, and more...
+            </h2>
+            <ul className='ml-4'>
+              <li className='flex m-2'>
+                <FaSearch />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Robust tagging and search system.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <FaYoutube />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Weekly additions from the newest video releases.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <GiSkateboard />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Created and curated by skaters.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <FaHeart />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Save your favorite spots.
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className='what-we-offer ml-2 mr-4 my-4 rounded-lg bg-slate-50 shadow-md'>
+            <h2 className='text-left'>
+              A collection of <NumberCountUp count={123} /> spots, and more...
+            </h2>
+            <ul className='ml-4'>
+              <li className='flex m-2'>
+                <FaSearch />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Robust tagging and search system.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <FaYoutube />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Weekly additions from the newest video releases.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <GiSkateboard />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Created and curated by skaters.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <FaHeart />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Save your favorite spots.
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className='what-we-offer ml-2 mr-4 my-4 rounded-lg bg-slate-50 shadow-md'>
+            <h2 className='text-left'>
+              A collection of <NumberCountUp count={123} /> spots, and more...
+            </h2>
+            <ul className='ml-4'>
+              <li className='flex m-2'>
+                <FaSearch />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Robust tagging and search system.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <FaYoutube />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Weekly additions from the newest video releases.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <GiSkateboard />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Created and curated by skaters.
+                </p>
+              </li>
+              <li className='flex m-2'>
+                <FaHeart />
+                <p className={`pl-1 ${merriweather.className}`}>
+                  Save your favorite spots.
+                </p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
---
+++ types/supabase.ts
@@ -0,0 +1,252 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
+
+export interface Database {
+  public: {
+    Tables: {
+      spot_likes: {
+        Row: {
+          created_at: string;
+          id: number;
+          spot_id: number | null;
+          user_id: string | null;
+        };
+        Insert: {
+          created_at?: string;
+          id?: number;
+          spot_id?: number | null;
+          user_id?: string | null;
+        };
+        Update: {
+          created_at?: string;
+          id?: number;
+          spot_id?: number | null;
+          user_id?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'spot_likes_spot_id_fkey';
+            columns: ['spot_id'];
+            isOneToOne: false;
+            referencedRelation: 'spots';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'spot_likes_user_id_fkey';
+            columns: ['user_id'];
+            isOneToOne: false;
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
+      spots: {
+        Row: {
+          city: string | null;
+          created_at: string;
+          description: string | null;
+          featured_in: number[] | null;
+          id: number;
+          image_links: string[] | null;
+          is_premium: boolean | null;
+          lat: number | null;
+          lng: number | null;
+          name: string | null;
+          status: string | null;
+          tags: string[] | null;
+          type: string | null;
+          youtube_links: string[] | null;
+        };
+        Insert: {
+          city?: string | null;
+          created_at?: string;
+          description?: string | null;
+          featured_in?: number[] | null;
+          id?: number;
+          image_links?: string[] | null;
+          is_premium?: boolean | null;
+          lat?: number | null;
+          lng?: number | null;
+          name?: string | null;
+          status?: string | null;
+          tags?: string[] | null;
+          type?: string | null;
+          youtube_links?: string[] | null;
+        };
+        Update: {
+          city?: string | null;
+          created_at?: string;
+          description?: string | null;
+          featured_in?: number[] | null;
+          id?: number;
+          image_links?: string[] | null;
+          is_premium?: boolean | null;
+          lat?: number | null;
+          lng?: number | null;
+          name?: string | null;
+          status?: string | null;
+          tags?: string[] | null;
+          type?: string | null;
+          youtube_links?: string[] | null;
+        };
+        Relationships: [];
+      };
+      user_profiles: {
+        Row: {
+          created_at: string;
+          id: string;
+          interval: string | null;
+          is_subscribed: boolean | null;
+          nick_name: string | null;
+        };
+        Insert: {
+          created_at?: string;
+          id: string;
+          interval?: string | null;
+          is_subscribed?: boolean | null;
+          nick_name?: string | null;
+        };
+        Update: {
+          created_at?: string;
+          id?: string;
+          interval?: string | null;
+          is_subscribed?: boolean | null;
+          nick_name?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'user_profiles_id_fkey';
+            columns: ['id'];
+            isOneToOne: true;
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
+      videos: {
+        Row: {
+          company: string | null;
+          created_at: string;
+          id: number;
+          release_date: string | null;
+          title: string | null;
+          youtube_link: string | null;
+        };
+        Insert: {
+          company?: string | null;
+          created_at?: string;
+          id?: number;
+          release_date?: string | null;
+          title?: string | null;
+          youtube_link?: string | null;
+        };
+        Update: {
+          company?: string | null;
+          created_at?: string;
+          id?: number;
+          release_date?: string | null;
+          title?: string | null;
+          youtube_link?: string | null;
+        };
+        Relationships: [];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+}
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database['public']['Tables'] & Database['public']['Views'])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+        Database[PublicTableNameOrOptions['schema']]['Views'])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database['public']['Tables'] &
+      Database['public']['Views'])
+  ? (Database['public']['Tables'] &
+      Database['public']['Views'])[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : never;
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database['public']['Tables']
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+  ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : never;
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database['public']['Tables']
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+  ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : never;
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database['public']['Enums']
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database['public']['Enums']
+  ? Database['public']['Enums'][PublicEnumNameOrOptions]
+  : never;
