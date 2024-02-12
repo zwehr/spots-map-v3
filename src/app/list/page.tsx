9f508246f19645e215ce796472d50a87a9cbf591
zwehr <zwehrmeister@gmail.com>
2024-02-12 01:44:18 -0500

    update styling - make options cards (still need images)

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -37,26 +37,36 @@ export default async function ListSelection() {
   return (
     <>
       <h1>Lists</h1>
-      <h2>
-        <Link href='/list/skate-spots' className='link'>
-          View Spots List
-        </Link>
-      </h2>
-      <h2>
-        <Link href='/list/skate-parks' className='link'>
-          View Parks List
-        </Link>
-      </h2>
-      <h2>
-        <Link href='/list/skate-shops' className='link'>
-          View Shops List
-        </Link>
-      </h2>
-      <h2>
-        <Link href='/list/skate-videos' className='link'>
-          View Videos List
-        </Link>
-      </h2>
+      <div className='flex justify-center bg-gray-200 max-w-2xl mx-auto rounded-md shadow-lg p-4'>
+        <div className='flex justify-center mx-auto p-2 space-x-4 text-xl'>
+          <Link href='/list/skate-spots' className='link'>
+            <div className='bg-gray-400 p-2 rounded-sm shadow-md hover:scale-105 transition-transform duration-300 ease-in-out'>
+              <div className='w-full h-24 rounded bg-gray-50'>[Spot Image]</div>
+              View Spots List
+            </div>
+          </Link>
+          <Link href='/list/skate-parks' className='link'>
+            <div className='bg-gray-400 p-2 rounded-sm shadow-md hover:scale-105 transition-transform duration-300 ease-in-out'>
+              <div className='w-full h-24 rounded bg-gray-50'>[Park Image]</div>
+              View Parks List
+            </div>
+          </Link>
+          <Link href='/list/skate-shops' className='link'>
+            <div className='bg-gray-400 p-2 rounded-sm shadow-md hover:scale-105 transition-transform duration-300 ease-in-out'>
+              <div className='w-full h-24 rounded bg-gray-50'>[Shop Image]</div>
+              View Shops List
+            </div>
+          </Link>
+          <Link href='/list/skate-videos' className='link'>
+            <div className='bg-gray-400 p-2 rounded-sm shadow-md hover:scale-105 transition-transform duration-300 ease-in-out'>
+              <div className='w-full h-24 rounded bg-gray-50'>
+                [Video Image]
+              </div>
+              View Videos List
+            </div>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
