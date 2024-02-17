4ef61ff4f9843d9aa2c5f079ae136e340a47ce89
zwehr <zwehrmeister@gmail.com>
2024-02-17 04:34:31 -0500

    rework image upload server action, tear down AddVideoForm and start from scratch (currently only file input in form)

--- src/components/forms/AddVideoForm.tsx
+++ src/components/forms/AddVideoForm.tsx
@@ -1,137 +1,89 @@
 'use client';
 
-import { FormEvent, useState } from 'react';
-import { BeatLoader } from 'react-spinners';
-import { oswald } from '@/app/fonts';
-import { insertVideo } from './actions';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { getSignedURL } from './actions';
 
 export default function AddVideoForm() {
-  const [title, setTitle] = useState<string>('');
-  const [company, setCompany] = useState<string | null>(null);
-  const [year, setYear] = useState<number>(2024);
-  const [youtubePageLink, setYoutubePageLink] = useState<string>('');
-  const [youtubeEmbedLink, setYoutubeEmbedLink] = useState<string>('');
-  const [userSubmitted, setUserSubmitted] = useState(false);
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [fileUrl, setFileUrl] = useState<string | undefined>(undefined);
+  const [userSubmitted, setUserSubmitted] = useState<boolean>(false);
 
-  const releaseYears = Array.from({ length: 45 }, (_, index) => 2024 - index); // 2024 to 1980 for select options
-
-  const handleYearChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedValue = parseInt(e.target.value, 10);
-    setYear(selectedValue);
+  // from Sam Meech-Ward's tutorial (https://www.nexttonone.lol/upload-s3)
+  const computeSHA256 = async (file: File) => {
+    const buffer = await file.arrayBuffer();
+    const hashBuffer = await crypto.subtle.digest('SHA-256', buffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('');
+    return hashHex;
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     setUserSubmitted(true);
     e.preventDefault();
 
-    insertVideo(title, company, year, youtubePageLink, youtubeEmbedLink)
-      .then((response) => {
-        if (response !== undefined) {
-          if ('error' in response) {
-            console.error(response.error);
-          } else {
-            console.log(response.data);
-            showSuccessToast();
-          }
-        } else {
-          console.error('Response is undefined');
+    try {
+      if (file) {
+        const checksum = await computeSHA256(file);
+        const signedURLResult = await getSignedURL(
+          file.type,
+          file.size,
+          checksum
+        );
+
+        if (signedURLResult.failure !== undefined) {
+          throw new Error(signedURLResult.failure);
         }
 
-        setUserSubmitted(false);
-      })
-      .catch((error) => {
-        console.error('Error occurred:', error);
-        setUserSubmitted(false);
-      });
+        const url = signedURLResult.success.url;
+        await fetch(url, {
+          method: 'PUT',
+          body: file,
+          headers: {
+            'Content-Type': file.type,
+          },
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setUserSubmitted(false);
+    }
+    // good place for toast message
+    console.log('made it to the end woohoo');
+    setUserSubmitted(false);
   };
 
-  const clearInputs = () => {
-    setTitle('');
-    setCompany(null);
-    setYear(2024);
-    setYoutubePageLink('');
-    setYoutubeEmbedLink('');
-  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    setFile(file);
 
-  const showSuccessToast = () => {
-    toast.success('Success Notification !', {});
+    if (fileUrl) {
+      URL.revokeObjectURL(fileUrl);
+    }
+
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setFileUrl(url);
+    } else {
+      setFileUrl(undefined);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor='title' className='uppercase font-bold'>
-        Title:
-      </label>
       <input
-        type='text'
-        name='title'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className='w-1/2'
-        required
+        type='file'
+        name='media'
+        accept='image/jpeg, image/png'
+        onChange={handleChange}
       />
-
-      <label htmlFor='company' className='uppercase font-bold'>
-        Company:
-      </label>
-      <input
-        type='text'
-        name='company'
-        value={company || ''}
-        onChange={(e) => setCompany(e.target.value)}
-        className='w-1/4 mb-0'
-      />
-      <p className='text-gray-600 italic mb-8'>
-        Optional - Leave blank if no company
-      </p>
-
-      <label htmlFor='year' className='uppercase font-bold'>
-        Year:
-      </label>
-      <select id='year' value={year} onChange={handleYearChange}>
-        {releaseYears.map((year) => (
-          <option key={year} value={year}>
-            {year}
-          </option>
-        ))}
-      </select>
-
-      <label htmlFor='youtubePageLink' className='uppercase font-bold'>
-        YouTube Link (Plain):
-      </label>
-      <input
-        type='text'
-        name='youtubePageLink'
-        value={youtubePageLink}
-        onChange={(e) => setYoutubePageLink(e.target.value)}
-        className='w-4/5'
-        required
-      />
-
-      <label htmlFor='' className='uppercase font-bold'>
-        YouTube Embed Link:
-      </label>
-      <input
-        type='text'
-        name=''
-        value={youtubeEmbedLink}
-        onChange={(e) => setYoutubeEmbedLink(e.target.value)}
-        className='w-4/5 mb-0'
-        required
-      />
-      <p className='mb-8 text-gray-600 italic'>
-        On YouTube video page, click <span className='font-bold'>Share</span>,
-        then <span className='font-bold'>Embed</span>, and then copy link from
-        src=&quot;<span className='font-bold'>...youtube.com/embed...</span>
-        &quot;.
-      </p>
-
-      <button
-        className={`uppercase mx-auto mt-5 mb-2 w-28 h-10 bg-green-400 rounded-lg hover:bg-green-500 hover:cursor-pointer shadow-md text-lg block ${oswald.className}`}
-      >
-        {userSubmitted ? <BeatLoader size={12} /> : 'Add Video'}
+      {fileUrl && file && (
+        <img src={fileUrl} alt='preview of selected file' className='w-24' />
+      )}
+      <button type='submit' className='bg-green-500 p-2 rounded'>
+        Post
       </button>
     </form>
   );
--- src/components/forms/actions.ts
+++ src/components/forms/actions.ts
@@ -1,72 +1,47 @@
 'use server';
 import supabase from '@/lib/utils/supabase';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand} from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import crypto from 'crypto';
 
-const s3 = new S3Client({
-  region: process.env.AWS_REGION!,
+// random 32 byte string for filename
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString("hex")
+
+const s3Client = new S3Client({
+  region: process.env.AWS_BUCKET_REGION!,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
-});
+})
 
-const acceptedTypes = ['image/jpeg', 'image/png'];
+const acceptedTypes = ["image/jpeg", "image/png"]
 
-const maxFileSize = 1024 * 1024 * 10; // Max 10 MB
+const maxFileSize = 1024 * 1024 * 3 // 3MB
 
-export async function getSignedURL(
-  type: string,
-  size: number,
-  checksum: string,
-  randName: string
-) {
-  // NOTE: Complete after auth is set up
-  // https://www.youtube.com/watch?v=t-lhgq7Nfpc&t=85s&ab_channel=SamMeech-Ward
-  if (true) {
-    if (!acceptedTypes.includes(type)) {
-      return { failure: 'Invalid file type' };
-    }
+export async function getSignedURL(fileType: string, size: number, checksum: string) {
+  if (!acceptedTypes.includes(fileType)) {
+    return {failure: 'Inavalid file type. JPEG or PNG accepted.'}
+  }
 
-    if (size > maxFileSize) {
-      return { failure: 'File too large' };
-    }
+  if (size > maxFileSize) {
+    return {failure: 'File too large - 3 MB maximum'}
+  }  
 
-    const PutObjCommand = new PutObjectCommand({
-      Bucket: process.env.AWS_BUCKET_NAME!,
-      Key: randName,
-      ContentType: type,
-      ContentLength: size,
-      ChecksumSHA256: checksum,
-    });
+  const putObjCommand = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME!,
+    Key: generateFileName(),
+    ContentType: fileType,
+    ContentLength: size,
+    ChecksumSHA256: checksum
+  }) 
 
-    const signedUrl = await getSignedUrl(s3, PutObjCommand, {
-      expiresIn: 3600,
-    });
-
-    return { success: { url: signedUrl, name: randName } };
-    // unreachable by design... for the moment
-  } else return { failure: 'Not authenticated' };
+  const signedURL = await getSignedUrl(s3Client, putObjCommand)
+  return {success: {url: signedURL}}
 }
 
 export async function getVideoTitles(query: string) {
   const { data, error } = await supabase.from('videos').select('id, title').filter('title', 'ilike', `%${query}%`).limit(5)
   console.log(data);
   return data
-}
-
-export async function insertVideo(title: string, company: string | null, year: number, youtubePageLink: string, youtubeEmbedLink: string) {
-  const { data, error } = await supabase
-  .from('videos')
-  .insert({title, company, release_year: year, youtube_link: youtubePageLink, youtube_embed_link: youtubeEmbedLink})
-  .select('id')
-
-  if (error) {
-    return {error: error.message}
-  }
-  if (data && data.length > 0) {
-    console.log(data);
-    return {data: data[0].id}
-  }
-  
 }
