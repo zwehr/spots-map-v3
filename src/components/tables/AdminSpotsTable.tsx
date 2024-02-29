bd0d77821d8c8ff3cf8612097c54868baac47b7b
zwehr <zwehrmeister@gmail.com>
2024-02-28 21:58:59 -0500

    temporarily remove admin spots table

--- src/components/tables/AdminSpotsTable.tsx
+++ src/components/tables/AdminSpotsTable.tsx
@@ -9,65 +9,28 @@ type AdminSpotsTableProps = {
 
 export default async function AdminSpotsTable(props: AdminSpotsTableProps) {
   return (
-    <table className='max-w-7xl mx-auto text-sm'>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Type</th>
-          <th>City</th>
-          <th>$$$</th>
-          <th>Clips</th>
-          <th>Add Clip</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.spots &&
-          props.spots.map((spot) => (
-            <tr key={spot._id}>
-              <td className='max-w-xs'>{spot.name}</td>
-              <td className='max-w-2xl'>{spot.description}</td>
-              <td>{spot.type[0].toUpperCase() + spot.type.slice(1)}</td>
-              <td>{spot.city}</td>
-              <td>{spot.isPremium ? 'Premium' : 'Free'}</td>
-              <td>
-                {spot.youtubeLinks.map((link, index) => (
-                  <div key={index} className='inline'>
-                    <a key={index} href={link} className='link'>
-                      {index + 1}
-                    </a>{' '}
-                  </div>
-                ))}
-              </td>
-              <td>
-                <button
-                  className='w-24 p-1 bg-slate-300 hover:bg-slate-400 rounded-lg shadow-md'
-                  onClick={() => alert('Not yet implemented.')}
-                >
-                  Add YT Link
-                </button>
-              </td>
-              <td>
-                <button
-                  className='w-12 p-1 bg-yellow-300 hover:bg-yellow-500 rounded-lg shadow-md'
-                  onClick={() => alert('Not yet implemented.')}
-                >
-                  Edit
-                </button>
-              </td>
-              <td>
-                <button
-                  className='w-12 p-1 bg-red-400 hover:bg-red-600 rounded-lg shadow-md'
-                  onClick={() => alert('Not yet implemented.')}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <>
+      {' '}
+      <table className='max-w-7xl mx-auto text-sm'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Type</th>
+            <th>City</th>
+            <th>$$$</th>
+            <th>Clips</th>
+            <th>Add Clip</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+      <p className='text-center mt-4 text-red-500 text-xl'>
+        [Removed Description from spots db table, so this needs to be updated to
+        work properly.]
+      </p>
+    </>
   );
 }
