db593719e5c8561c54442ddcf73bf2c155ee42b7
zwehr <zwehrmeister@gmail.com>
2024-03-07 14:18:23 -0500

    remove old reference to MongoDB helper function to fix error

--- src/app/list/skate-spots/page.tsx
+++ src/app/list/skate-spots/page.tsx
@@ -1,30 +1,15 @@
-import LinkTags from '@/components/tags/LinkTags';
-import getAllSpots from '@/lib/fetch/getAllSpots';
 import Link from 'next/link';
 
 export default async function SpotsList() {
-  const spotsData: Promise<Spot[] | undefined> = getAllSpots();
-  const spots = await spotsData;
-
   return (
     <>
       <h1>Spots List</h1>
-      <table className='max-w-6xl mt-6 mx-auto'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Type</th>
-            <th>City</th>
-            <th>Tags</th>
-            <th>More</th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
       <p className='text-xl text-red-500 text-center mt-12'>
-        [Description removed from spots, other fields added... will be updated
-        later]
+        [Will be updated to match{' '}
+        <Link href='/list/skate-videos' className='link'>
+          public videos list style
+        </Link>
+        ]
       </p>
     </>
   );
