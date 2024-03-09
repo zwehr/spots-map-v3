e30f6a43e935673f8cf598308fc8a099b9f74a0c
zwehr <zwehrmeister@gmail.com>
2024-03-09 02:05:52 -0500

    add icons (comically large at this point)

--- src/components/map/MapFilter.tsx
+++ src/components/map/MapFilter.tsx
@@ -3,6 +3,10 @@
 import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import iconMarker from 'leaflet/dist/images/marker-icon.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import { Database } from '../../../types/supabase';
 
 type Spot = Database['public']['Tables']['spots']['Row'];
@@ -18,6 +22,12 @@ type MapListProps = {
 
 export default function MapMain({ spots, mapOptions }: MapListProps) {
   const [selectedSpot, setSelectedSpot] = useState<Spot | null>(null);
+
+  const icon = L.icon({
+    iconRetinaUrl: iconRetina.src,
+    iconUrl: iconMarker.src,
+    shadowUrl: iconShadow.src,
+  });
 
   return (
     <div className='h-full w-full bg-gray-100 relative'>
@@ -35,6 +45,7 @@ export default function MapMain({ spots, mapOptions }: MapListProps) {
             <Marker
               key={spot.id}
               position={[spot.lat!, spot.lng!]}
+              icon={icon}
               eventHandlers={{
                 click: () => {
                   console.log(spot.name);
