2342471b82dc8fe203b8d9701bfd98bf49657227
zwehr <zwehrmeister@gmail.com>
2024-03-10 18:55:02 -0400

    insert spots_videos M:M entries after inserting spot

--- src/app/admin/add/spot/page.tsx
+++ src/app/admin/add/spot/page.tsx
@@ -19,22 +19,70 @@ export default function AddSpot() {
     'use server';
     const supabase = createServerComponentClient({ cookies });
 
+    // Initialize with a default value
+    // Will reassign after inserting spot, use for spots_videos M:M insert(s)
+    let newSpotId: number = -1;
+
+    // first create the new spot in spots table, selecting for new id
     try {
-      const { error } = await supabase.from('spots').insert({
-        name: name,
-        lat: lat,
-        lng: lng,
-        city: city,
-        country: country,
-        type: type,
-        is_premium: isPremium,
-        status: status,
-        tags: tags,
-        image_links: image_links,
-        featured_in: featured_in,
-      });
+      const { data, error } = await supabase
+        .from('spots')
+        .insert({
+          name: name,
+          lat: lat,
+          lng: lng,
+          city: city,
+          country: country,
+          type: type,
+          is_premium: isPremium,
+          status: status,
+          tags: tags,
+          image_links: image_links,
+          featured_in: featured_in,
+        })
+        .select();
+
+      if (error) {
+        console.error('Error inserting to spots table:', error.message);
+        return;
+      }
+
+      if (data !== null) {
+        // Use type assertion to convert data to an array
+        const dataArray: any[] = data;
+
+        if (dataArray.length > 0) {
+          const insertedRecord = dataArray[0];
+          const insertedId = insertedRecord.id;
+          newSpotId = insertedId;
+        } else {
+          console.log('No new spot data inserted');
+        }
+      } else {
+        console.log('No new spot data returned from database');
+      }
     } catch (error) {
       console.log(error);
+      return;
+    }
+
+    // create spots_videos M:M inserts based on all videos in featured_in
+    for (const videoId of featured_in) {
+      console.log(videoId, newSpotId);
+      try {
+        const { error } = await supabase.from('spots_videos').insert({
+          video_id: videoId,
+          spot_id: newSpotId,
+        });
+        if (error) {
+          console.error(
+            'Error inserting to spots_videos table:',
+            error.message
+          );
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
