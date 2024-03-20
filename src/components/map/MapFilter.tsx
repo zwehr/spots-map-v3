edec46f4f0178477b5114bb7fb3ec4e8c25418c3
zwehr <zwehrmeister@gmail.com>
2024-03-20 18:18:09 -0400

    remove unnecessary useState, map Popup imports

--- src/components/map/MapFilter.tsx
+++ src/components/map/MapFilter.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import iconMarker from 'leaflet/dist/images/marker-icon.png';
